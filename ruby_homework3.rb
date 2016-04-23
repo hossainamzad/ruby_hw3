e23d88e75dc979d7b4742f2513cbbbda75544f61
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2016-04-23 15:32:46 -0400

    making some changes

--- ruby_homework3.rb
+++ ruby_homework3.rb
@@ -9,47 +9,29 @@ end
 rotate(3)
 
 
+def rotate(n)
+	array = [5,6,9,54,10]
+	n_array = []
+	for i in 0..n-1
+		n_array[i] = array[i]
+	end
+	array.shift(n)
+	n_array.each do |item|
+		array.push(item)
+	end
+	puts array		
+end	
+rotate(4)
 
 
 
+def fact(n)
+	if n == 0
+		1
+	else
+		f = n * fact(n-1)
+	end
+end	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def rotate(n)
-# 	array = [5,6,9,54,10]
-# 	n_array = []
-# 	for i in 0..n-1
-# 		n_array[i] = array[i]
-# 	end
-# 	array.shift(n)
-# 	n_array.each do |item|
-# 		array.push(item)
-# 	end
-# 	puts array		
-# end	
-
-# def fact(n)
-# 	if n == 0
-# 		1
-# 	else
-# 		f = n * fact(n-1)
-# 	end
-# end	
-
-# fact (5)
+fact (5)
 
